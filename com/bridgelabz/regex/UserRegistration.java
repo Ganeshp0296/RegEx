1e1d10c1c6b773636b7b5643b378444d22f2aed8
Ganeshp0296 <Ganeshp457@gmail.com>
2022-06-21 23:01:14 +0530

    Adding UC4

--- com/bridgelabz/regex/UserRegistration.java
+++ com/bridgelabz/regex/UserRegistration.java
@@ -47,12 +47,28 @@ public class UserRegistration {
 				System.out.println("Pattern not matched");
 		}
 		
+		
+		private void mobileValidation() {
+			String mNumber;
+			Scanner get = new Scanner(System.in);
+			System.out.println("Enter Mobile Number : \t Eg:91 8542365232");
+			mNumber = get.next();
+			String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(mNumber);
+			if (matcher.find())
+				System.out.println("Pattern matched");
+			else
+				System.out.println("Pattern not matched");
+		}
+		
 		public static void main(String[] args) {
 			System.out.println("Welcome to regex");
 			UserRegistration userRegestration = new UserRegistration();
 			userRegestration.firstName();
 			userRegestration.lastName();
 			userRegestration.email();
+			userRegestration.mobileValidation();
 			
 		}
 	}
