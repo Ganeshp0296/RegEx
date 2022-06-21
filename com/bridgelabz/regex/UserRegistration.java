ab17b7d0fa5ff9a50c17246d5e6b67783f7ae697
Ganeshp0296 <Ganeshp457@gmail.com>
2022-06-21 22:45:02 +0530

    Adding UC1

---
+++ com/bridgelabz/regex/UserRegistration.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+		String firstName;
+		public void firstName() {
+			Scanner get = new Scanner(System.in);
+			System.out.println("Enter Name: \t note:First Latter should capital");
+			firstName = get.next();
+	        String regex = "^[A-Z]{1}[a-z]{2,}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(firstName);
+	        if (matcher.find())
+				System.out.println("Pattern matched");
+			else
+				System.out.println("Pattern not matched");
+	    }
+		public static void main(String[] args) {
+			System.out.println("Welcome to regex");
+			UserRegistration userRegestration = new UserRegistration();
+			userRegestration.firstName();
+		}
+	}
+
+	
+
+
